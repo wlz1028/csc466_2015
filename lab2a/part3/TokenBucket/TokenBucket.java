b2d785f2895bfc65fe38339c91f522f47d6afd72
qyguan <summerr.guan@gmail.com>
2015-02-03 17:15:40 -0500

    update source

--- lab2a/part3/TokenBucket/TokenBucket.java
+++ lab2a/part3/TokenBucket/TokenBucket.java
@@ -33,6 +33,17 @@ public class TokenBucket implements Runnable
 	 * @param bucketRate Token generating rate in tokens/sec.
 	 * @param fileName Name of file token bucket uses to record packet arrivals.
 	 */
+        public static void main(String[] args){
+        // listen on port 4444, send to localhost:4445,
+         // max. size of received packet is 1024 bytes,
+        // buffer capacity is 100*1024 bytes,
+        // token bucket has 10000 tokens, rate 5000 tokens/sec, and
+        // records packet arrivals to bucket.txt).
+         TokenBucket lb = new TokenBucket(4444, "localhost", 4445,
+         1024, 100*1024, 10000, 5000, "bucket.txt");
+         new Thread(lb).start();
+        }
+
 	public TokenBucket(int inPort, String outAddress, int outPort, 
 			int maxPacketSize, long bufferCapacity,
 			int bucketSize, int bucketRate, String fileName)
